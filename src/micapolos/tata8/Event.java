73d2c68707d045b10022f8861345a973d8455e3a
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-08-30 14:08:48 +0200

    event on

--- src/micapolos/tata8/Event.java
+++ src/micapolos/tata8/Event.java
@@ -1,9 +1,11 @@
 package micapolos.tata8;
 
+import java.util.function.BooleanSupplier;
+
 public interface Event {
   boolean didHappen();
 
-  static Event on(Event event) {
-    return () -> event.didHappen();
+  static Event on(BooleanSupplier supplier) {
+    return () -> supplier.getAsBoolean();
   }
 }
