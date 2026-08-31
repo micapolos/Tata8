71e6c454259d1a12a1a1e3a27e2e2fd6d392d08f
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-08-31 20:17:47 +0200

    Event2

--- src/micapolos/tata8/model/HorizontalDirection.java
+++ src/micapolos/tata8/model/HorizontalDirection.java
@@ -2,18 +2,23 @@ package micapolos.tata8.model;
 
 import static micapolos.tata8.model.Clip.on;
 import static micapolos.tata8.model.Clip.select;
+import static micapolos.tata8.model.Clipped.clipped;
 
 public enum HorizontalDirection {
   LEFT, RIGHT;
 
   public static Clipped<Value<HorizontalDirection>> fromSpans(Span leftSpan, Span rightSpan) {
     Value<HorizontalDirection> direction = Value.variable();
-    return Clipped.clipped(
+    return clipped(
       direction.toValue(),
       select(
-        on(Key.LEFT.press, direction.set(HorizontalDirection.LEFT)),
-        on(Key.LEFT.release, direction.set(Key.RIGHT.isPressed.select(HorizontalDirection.RIGHT, HorizontalDirection.LEFT))),
-        on(Key.RIGHT.press, direction.set(HorizontalDirection.RIGHT)),
-        on(Key.RIGHT.release, direction.set(Key.LEFT.isPressed.select(HorizontalDirection.LEFT, HorizontalDirection.RIGHT)))));
+        on(leftSpan.start, direction.set(HorizontalDirection.LEFT)),
+        on(leftSpan.end, direction.set(rightSpan.isInside.select(HorizontalDirection.RIGHT, null))),
+        on(rightSpan.start, direction.set(HorizontalDirection.RIGHT)),
+        on(rightSpan.end, direction.set(leftSpan.isInside.select(HorizontalDirection.LEFT, null)))));
+  }
+
+  static void main() {
+    fromSpans(Key.LEFT.pressedSpan(), Key.RIGHT.pressedSpan()).show();
   }
 }
--- src/micapolos/tata8/model/VerticalDirection.java
+++ src/micapolos/tata8/model/VerticalDirection.java
@@ -2,7 +2,23 @@ package micapolos.tata8.model;
 
 import static micapolos.tata8.model.Clip.on;
 import static micapolos.tata8.model.Clip.select;
+import static micapolos.tata8.model.Clipped.clipped;
 
 public enum VerticalDirection {
   UP, DOWN;
+
+  public static Clipped<Value<VerticalDirection>> fromSpans(Span upSpan, Span downSpan) {
+    var direction = Value.<VerticalDirection>variable();
+    return clipped(
+      direction.toValue(),
+      select(
+        on(upSpan.start, direction.set(VerticalDirection.UP)),
+        on(upSpan.end, direction.set(downSpan.isInside.select(VerticalDirection.DOWN, null))),
+        on(downSpan.start, direction.set(VerticalDirection.DOWN)),
+        on(downSpan.end, direction.set(upSpan.isInside.select(VerticalDirection.UP, null)))));
+  }
+
+  static void main() {
+    fromSpans(Key.UP.pressedSpan(), Key.DOWN.pressedSpan()).show();
+  }
 }
