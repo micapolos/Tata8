4948408a767df99ddc1cef681b88d4194e0d9ff3
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-08-31 13:35:42 +0200

    model

--- src/micapolos/tata8/model/Game.java
+++ src/micapolos/tata8/model/Game.java
@@ -8,11 +8,22 @@ import static micapolos.tata8.Game.onUpdate;
 import static micapolos.tata8.model.Number.number;
 
 public final class Game {
-  static final List<Clip> clips = new ArrayList<>();
+  static final List<Action> initActions = new ArrayList<>();
+  static final List<Animation> animations = new ArrayList<>();
+
   static final Size size = new Size(number(micapolos.tata8.Game.size.width), number(micapolos.tata8.Game.size.height));
 
+  static void addInitAction(Action initAction) {
+    initActions.add(initAction);
+  }
+
+  static void add(Animation animation) {
+    animations.add(animation);
+  }
+
   static void add(Clip clip) {
-    clips.add(clip);
+    initActions.add(clip::start);
+    animations.add(clip::advance);
   }
 
   public static final Mouse mouse = new Mouse();
@@ -39,18 +50,18 @@ public final class Game {
 
   public static void show() {
     startedValue = true;
-    for (Clip clip : clips) {
-      clip.start();
+    for (Action initAction : initActions) {
+      initAction.execute();
     }
     onUpdate = () -> {
       if (keys.reset.pressed()) {
         startedValue = true;
-        for (Clip clip : clips) {
-          clip.start();
+        for (Action initAction : initActions) {
+          initAction.execute();
         }
       }
-      for (Clip clip : clips) {
-        clip.advance(1/60f);
+      for (Animation animation : animations) {
+        animation.update(1/60f);
       }
       startedValue = false;
     };
