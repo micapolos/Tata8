e7b83ba987742b3fabf390660169a5326e68031e
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-02 01:23:05 +0200

    number format 3 digits after dot

--- src/micapolos/tata8/model/Sprite.java
+++ src/micapolos/tata8/model/Sprite.java
@@ -1,13 +1,14 @@
 package micapolos.tata8.model;
 
-import static micapolos.Blocks.ifNotNull;
-import static micapolos.tata8.model.Anchor.anchor;
-import static micapolos.tata8.model.Anchor.topLeftAnchor;
+import static micapolos.Blocks.*;
+import static micapolos.tata8.model.Anchor.*;
 import static micapolos.tata8.model.Clip.*;
-import static micapolos.tata8.model.Flip.noFlip;
-import static micapolos.tata8.model.Image.image;
-import static micapolos.tata8.model.Position.position;
-import static micapolos.tata8.model.Value.nullValue;
+import static micapolos.tata8.model.Depth.*;
+import static micapolos.tata8.model.Flip.*;
+import static micapolos.tata8.model.Image.*;
+import static micapolos.tata8.model.Number.*;
+import static micapolos.tata8.model.Position.*;
+import static micapolos.tata8.model.Value.*;
 
 public final class Sprite extends Component {
   final micapolos.tata8.Sprite state;
@@ -15,12 +16,14 @@ public final class Sprite extends Component {
   public final Anchor anchor;
   public final Position position;
   public final Flip flip;
+  public final Depth depth;
 
   Sprite(Clip clip,
          Value<Image> image,
          Anchor anchor,
          Position position,
          Flip flip,
+         Depth depth,
          micapolos.tata8.Sprite state) {
     super(clip);
     this.state = state;
@@ -28,6 +31,7 @@ public final class Sprite extends Component {
     this.anchor = anchor;
     this.position = position;
     this.flip = flip;
+    this.depth = depth;
   }
 
   @Override
@@ -55,27 +59,31 @@ public final class Sprite extends Component {
 
   public static Sprite newSprite() {
     micapolos.tata8.Sprite state = micapolos.tata8.Game.newSprite();
-    return new Sprite(Clip.emptyClip, nullValue(), topLeftAnchor, position(0, 0), noFlip, state);
+    return new Sprite(Clip.emptyClip, nullValue(), topLeftAnchor, position(0, 0), noFlip, depth(0), state);
   }
 
   public Sprite with(Clip clip) {
-    return new Sprite(clip, image, anchor, position, flip, state);
+    return new Sprite(clip, image, anchor, position, flip, depth, state);
   }
 
   public Sprite with(Value<Image> image) {
-    return new Sprite(clip, image, anchor, position, flip, state);
+    return new Sprite(clip, image, anchor, position, flip, depth, state);
   }
 
   public Sprite with(Anchor anchor) {
-    return new Sprite(clip, image, anchor, position, flip, state);
+    return new Sprite(clip, image, anchor, position, flip, depth, state);
   }
 
   public Sprite with(Position position) {
-    return new Sprite(clip, image, anchor, position, flip, state);
+    return new Sprite(clip, image, anchor, position, flip, depth, state);
   }
 
   public Sprite with(Flip flip) {
-    return new Sprite(clip, image, anchor, position, flip, state);
+    return new Sprite(clip, image, anchor, position, flip, depth, state);
+  }
+
+  public Sprite with(Depth depth) {
+    return new Sprite(clip, image, anchor, position, flip, depth, state);
   }
 
   @Override
@@ -84,13 +92,14 @@ public final class Sprite extends Component {
   }
 
   static void main() {
-    Number x = Number.newNumber(160);
+    Number x = newNumber(160);
 
     newSprite()
       .with(image(Game.class, "depressedChicken.png").sliceVertically(8).get(0))
       .with(anchor(16, 16))
       .with(position(x, 128))
       .with(frame(1/60f, x.add(1)).repeat())
+      .with(depth(1))
       .show();
   }
 }
