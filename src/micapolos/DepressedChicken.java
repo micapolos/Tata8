827332413204ecd63968adb6fc397d7e844357b7
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-06 12:29:15 +0200

    sad

---
+++ src/micapolos/DepressedChicken.java
@@ -0,0 +1,8 @@
+package micapolos;
+
+import micapolos.tata8.Game;
+import micapolos.tata8.Image;
+
+public final class DepressedChicken {
+  public static final Image image = Game.loadImage(DepressedChicken.class, "depressedChicken.png");
+}
