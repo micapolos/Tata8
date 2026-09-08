2e04bb91289d38b8bf5effa6c7b16c59c02ac132
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-08 07:47:06 +0200

    sad

--- src/micapolos/zexy3/runtime/Box.kt
+++ src/micapolos/zexy3/runtime/Box.kt
@@ -1,6 +1,6 @@
 package micapolos.zexy3.runtime
 
-class Box<T>(var supplier: (() -> T)? = null, var defaultValue: T) {
+class Box<T>(var defaultValue: T, var supplier: (() -> T)? = null) {
   var value
     get() = if (supplier != null) supplier!!() else defaultValue
     set(v: T) {
