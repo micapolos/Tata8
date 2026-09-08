be7e9097b7e66d50422d45e04c72e7d37ac72974
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-08 10:14:36 +0200

    sad

--- src/micapolos/zexy3/Variable.kt
+++
@@ -1,7 +0,0 @@
-package micapolos.zexy3
-
-class Variable<T: Value<T>>(val block: Block<*>, val initial: T): Value<T>
-
-fun <T: Value<T>> Block<*>.newVariable(initial: T): Variable<T> = Variable(this, initial)
-
-fun Block<*>.newVariable2(initial: Double): Variable<Number> = newVariable<Number>(number(initial))
