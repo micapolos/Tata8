a9fa0b629279cf0ac456ec33cae65b47773eb425
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-08 10:19:23 +0200

    sad

--- src/micapolos/zexy3/Block.kt
+++ src/micapolos/zexy3/Block.kt
@@ -1,23 +1,21 @@
 package micapolos.zexy3
 
-sealed class Block<T: Value<T>> {
-  object Root: Block<Void>()
-  class Child<T: Value<T>>(val parent: Block<*>): Block<T>()
-}
+class Block<T: Value<T>>(val parent: Block<*>? = null)
 
-fun <T: Value<T>> main(fn: Block<Void>.() -> Void): Void =
-  Block.Root.fn()
+fun <T: Value<T>> main(fn: Block<*>.() -> T): T =
+  Block().fn()
 
 fun <T: Value<T>> Block<*>.block(fn: Block<*>.() -> T): T =
-  Block.Child(this).fn()
+  Block(this).fn()
 
 fun main() {
   main {
-    val x = block {
+    val z = block {
       val x = newVariable(0.0)
       val y = newVariable(1.0)
-      x
+      x + y
     }
-    Void
-  }
+    val z2 = z + z
+    z2
+  } + 3.0
 }
