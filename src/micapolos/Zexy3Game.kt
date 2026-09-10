3755def958a4898da6265303856876d87c47a2da
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-10 19:59:16 +0200

    sad

--- src/micapolos/Zexy3Game.kt
+++ src/micapolos/Zexy3Game.kt
@@ -5,8 +5,7 @@ import micapolos.zexy3.*
 fun main() {
   val x = variable(10)
   game
-    .withResources(Sandbox::class)
     .withTitle("My first game")
-    .with(sprite.with(image("/micapolos/socksgirl-sheet.png")))
+    .with(sprite.with(image("/micapolos/depressedChicken.png")))
     .show()
 }
