561787723da5250ccc65482e319fdcdbebd53592
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-10 19:12:11 +0200

    sad

--- src/micapolos/zexy3/Image.kt
+++ src/micapolos/zexy3/Image.kt
@@ -17,3 +17,7 @@ val Value<Image>.height get() = Integer(ModelInteger.ImageHeight(modelImage))
 
 fun Value<Image>.sprite(x: Int, y: Int) = sprite(this, x.value, y.value)
 fun Value<Image>.sprite(x: Value<Integer>, y: Value<Integer>) = sprite(this, x, y)
+
+fun Value<Image>.show() {
+  sprite.with(this).show()
+}
