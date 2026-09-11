7af5ba3a705ddb8b607d290abb6d73c8d6b7160d
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-11 22:39:17 +0200

    sad

--- src/micapolos/zexy3/compiler/Value.kt
+++ src/micapolos/zexy3/compiler/Value.kt
@@ -86,8 +86,30 @@ fun <T : Value<T>> Compiler.animated(value: Value<T>): Animated<*> =
       )
     }
 
-    is Value.StartWhen -> TODO()//evaluator(value.value)
-    is Value.Stretch -> TODO()//evaluator(value.value)
+    is Value.StartWhen -> {
+      val animatedCondition = animated(value.condition)
+      val animatedValue = animated(value.value)
+      Animated(
+        animatedValue.evaluator,
+        parallel(
+          animatedCondition.animation,
+          animation(value.value).startWhenNotZero(animatedCondition.evaluator as IntEvaluator)
+        )
+      )
+    }
+
+    is Value.Stretch -> {
+      val animatedValue = animated(value.value)
+      val animatedFactor = animated(value.factor)
+      Animated(
+        animatedValue.evaluator,
+        parallel(
+          animatedFactor.animation,
+          animatedValue.animation.stretch(animatedFactor.evaluator as DoubleEvaluator)
+        )
+      )
+    }
+
     is Value.Stateful -> {
       val animatedState = animated(value.state)
       val animatedValue = animated(value.value)
@@ -106,67 +128,5 @@ fun <T : Value<T>> Compiler.animated(value: Value<T>): Animated<*> =
     }
   }
 
-fun <T : Value<T>> Compiler.evaluator(value: Value<T>): Evaluator<*> =
-  when (value) {
-    is Variable -> variableEvaluator(value)
-    is Integer -> integerEvaluator(value)
-    is Number -> numberEvaluator(value)
-    is Text -> textEvaluator(value)
-    is Color -> colorEvaluator(value)
-    is Image -> imageEvaluator(value)
-    is Font -> fontEvaluator(value)
-    is Drawing -> drawingEvaluator(value)
-    is Void -> voidEvaluator(value)
-
-    is Value.Logged -> evaluator(value.value).logged(value.label)
-    is Value.RunWhile -> TODO()//evaluator(value.value)
-    is Value.Select -> TODO()
-    is Value.Sequence -> TODO()
-    is Value.StartWhen -> TODO()//evaluator(value.value)
-    is Value.Stretch -> TODO()//evaluator(value.value)
-    is Value.Stateful -> TODO()
-    is Value.Race -> TODO()
-    is Value.Frame -> animated(value).evaluator
-  }
-
-fun Compiler.intEvaluator(value: Value<Integer>): IntEvaluator =
-  animated(value).evaluator as IntEvaluator
-
-fun Compiler.doubleEvaluator(value: Value<Number>): DoubleEvaluator =
-  animated(value).evaluator as DoubleEvaluator
-
-fun <T : Value<T>> Compiler.objectEvaluator(value: Value<T>): ObjectEvaluator<*> =
-  animated(value).evaluator as ObjectEvaluator<*>
-
-fun <T : Value<T>> Compiler.animation(value: Value<T>): Animation =
-  when (value) {
-    is Variable -> variableAnimation(value)
-    is Integer -> integerAnimation(value)
-    is Number -> numberAnimation(value)
-    is Color -> colorAnimation(value)
-    is Text -> textAnimation(value)
-    is Image -> imageAnimation(value)
-    is Font -> fontAnimation(value)
-    is Drawing -> drawingAnimation(value)
-    is Void -> voidAnimation(value)
-
-    is Value.Logged -> animation(value.value)
-    is Value.RunWhile ->
-      animation(value.value)
-        .runWhileNotZero(intEvaluator(value.condition))
-
-    is Value.Select -> TODO()
-    is Value.Sequence -> TODO()
-
-    is Value.StartWhen ->
-      animation(value.value)
-        .startWhenNotZero(intEvaluator(value.condition))
-
-    is Value.Stretch ->
-      animation(value.value)
-        .stretch(doubleEvaluator(value.factor))
-
-    is Value.Stateful -> TODO()
-    is Value.Race -> TODO()
-    is Value.Frame -> animated(value).animation
-  }
+fun <T : Value<T>> Compiler.evaluator(value: Value<T>): Evaluator<*> = TODO()
+fun <T : Value<T>> Compiler.animation(value: Value<T>): Animation = TODO()
