4f08232aa7270b3db0da52c5cccc4b870c91d381
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-11 02:59:12 +0200

    sad

--- src/micapolos/zexy3/examples/Frame.kt
+++ src/micapolos/zexy3/examples/Frame.kt
@@ -3,9 +3,5 @@ package micapolos.zexy3.examples
 import micapolos.zexy3.*
 
 fun main() {
-  rect
-    .with(position(10, 10))
-    .with(size(frame.count, 10))
-    .with(color.red)
-    .show()
+  frame.count.show()
 }
