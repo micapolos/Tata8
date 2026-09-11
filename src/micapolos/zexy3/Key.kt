c9eeb8f066e7eacf9313091c35f45b9517d3b685
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-11 07:09:02 +0200

    sad

---
+++ src/micapolos/zexy3/Key.kt
@@ -0,0 +1,23 @@
+package micapolos.zexy3
+
+import micapolos.zexy3.model.Integer.KeyDown as ModelKeyDown
+import micapolos.zexy3.model.Key as ModelKey
+
+class Key internal constructor(isPressed: Value<Bool>): Value<Key>(listOf(isPressed)) {
+  companion object {
+    val left = key(ModelKey.LEFT)
+    val right = key(ModelKey.RIGHT)
+    val up = key(ModelKey.UP)
+    val down = key(ModelKey.DOWN)
+    val z = key(ModelKey.Z)
+    val x = key(ModelKey.X)
+
+    internal fun key(model: ModelKey) = key(Bool(Integer(ModelKeyDown(model))))
+  }
+}
+
+val Value<Key>.isPressed get() = children[0] as Value<Bool>
+
+fun key(isPressed: Value<Bool>): Value<Key> = Key(isPressed)
+
+val key = Key.Companion
---
+++ src/micapolos/zexy3/examples/Key.kt
@@ -0,0 +1,7 @@
+package micapolos.zexy3.examples
+
+import micapolos.zexy3.*
+
+fun main() {
+  key.left.isPressed.show()
+}
