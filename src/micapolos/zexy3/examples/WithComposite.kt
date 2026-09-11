3c4db48cd17045425da4d975b2fc12f4f089546a
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-11 12:52:44 +0200

    sad

--- src/micapolos/zexy3/examples/WithComposite.kt
+++ src/micapolos/zexy3/examples/WithComposite.kt
@@ -6,8 +6,20 @@ fun main() {
   stack(
     rect
       .with(position(8, 8))
-      .with(size(256, 96))
+      .with(size(64, 96))
+      .with(color.black),
+    rect
+      .with(position(72, 8))
+      .with(size(64, 96))
       .with(color(0.5, 0.2, 0.3, 1.0)),
+    rect
+      .with(position(136, 8))
+      .with(size(64, 96))
+      .with(color(0.8, 0.9, 0.5, 1.0)),
+    rect
+      .with(position(200, 8))
+      .with(size(64, 96))
+      .with(color.white),
     sprite
       .with(image("/micapolos/depressedChicken.png"))
       .with(position(8, 8)),
