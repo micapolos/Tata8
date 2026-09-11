9d1768967cb39abf27e5170d7dde5cdd5b75151f
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-11 02:51:47 +0200

    sad

--- src/micapolos/zexy3/examples/Rect.kt
+++ src/micapolos/zexy3/examples/Rect.kt
@@ -3,5 +3,9 @@ package micapolos.zexy3.examples
 import micapolos.zexy3.*
 
 fun main() {
-  rect(10, 10, 30, 30).with(color.green).show()
+  rect
+    .with(position(10, 10))
+    .with(size(30, 30))
+    .with(color.red)
+    .show()
 }
--- src/micapolos/zexy3/examples/Stack.kt
+++ src/micapolos/zexy3/examples/Stack.kt
@@ -6,9 +6,14 @@ fun main() {
   stack(
     label
       .with("Hello, world!")
-      .with(position(10, 10)),
+      .with(position(10, 10))
+      .with(color.yellow),
     sprite
       .with(image("/micapolos/depressedChicken.png"))
-      .with(position(10, 30))
+      .with(position(10, 30)),
+    rect
+      .with(position(10, 80))
+      .with(size(100, 20))
+      .with(color.green)
   ).show()
 }
