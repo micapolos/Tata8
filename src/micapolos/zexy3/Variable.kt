f1821d56797d91811f6de3624ee1636c7dbd12a0
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-12 18:42:23 +0200

    sad

--- src/micapolos/zexy3/Variable.kt
+++ src/micapolos/zexy3/Variable.kt
@@ -1,11 +1,8 @@
 package micapolos.zexy3
 
 import micapolos.zexy3.model.Variable as ModelVariable
-import micapolos.zexy3.model.Void as ModelVoid
 
 open class Variable<T : Value<T>> internal constructor(modelVariable: ModelVariable<*>): Value<T>(modelVariable)
-
-internal val Variable<*>.erasedModelVariable get() = model as ModelVariable<ModelVoid>
 
 fun <T : Value<T>> variable(initial: Value<T>): Variable<T> = Variable(ModelVariable(initial.model))
 
--- src/micapolos/zexy3/compiler/Game.kt
+++ src/micapolos/zexy3/compiler/Game.kt
@@ -2,8 +2,32 @@ package micapolos.zexy3.compiler
 
 import micapolos.zexy3.indexed.Game
 import micapolos.zexy3.runtime.Animated
+import micapolos.zexy3.runtime.Animation
 import micapolos.zexy3.runtime.Drawing
 import micapolos.zexy3.runtime.Game as RuntimeGame
 
-fun Compiler.compile(game: Game): RuntimeGame =
-  RuntimeGame(game.title, animated(game.drawing) as Animated<Drawing>)
+fun Compiler.compile(game: Game): RuntimeGame {
+  val animatedDrawing = animated(game.drawing) as Animated<Drawing>
+  val drawingAnimation = animatedDrawing.animation
+  val initialIntArray = intArray.copyOf()
+  val initialDoubleArray = doubleArray.copyOf()
+  val initialObjectArray = objectArray.copyOf()
+  val initialAnimatedValues = animatedValues.toList()
+  return RuntimeGame(
+    game.title,
+    Animated(
+      animatedDrawing.evaluator,
+      object : Animation {
+        override fun start() {
+          for (i in animatedValues.indices) {
+            animatedValues[i] = initialAnimatedValues[i]
+          }
+          drawingAnimation.start()
+        }
+
+        override fun step(seconds: Double): Double {
+          return drawingAnimation.step(seconds)
+        }
+      })
+  )
+}
