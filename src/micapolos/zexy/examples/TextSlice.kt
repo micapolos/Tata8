e61634989108f69606ac48b48ac3ef437b6ea90a
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-13 17:30:27 +0200

    point / line

--- src/micapolos/zexy/examples/TextSlice.kt
+++ src/micapolos/zexy/examples/TextSlice.kt
@@ -3,9 +3,11 @@ package micapolos.zexy.examples
 import micapolos.zexy.*
 
 fun main() {
-  val text = "This is a very long text, which will slowly reveal itself."
+  val text = """
+    This is a very long text,
+    and it will slowly reveal itself.
+    How do you like it?
+  """.trimIndent()
 
-  text
-    .slice(0, frame.count.div(5).max(text.length))
-    .show()
+  text.slice(0, frame.count.div(5).max(text.length)).show()
 }
