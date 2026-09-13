dd0d1fc7802202a69340681acf3559f3b448ddc9
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-14 01:06:55 +0200

    point / line

---
+++ src/micapolos/zexy/compiler/Action.kt
@@ -0,0 +1,13 @@
+package micapolos.zexy.compiler
+
+import micapolos.zexy.runtime.*
+import micapolos.zexy.indexed.Action as IndexedAction
+
+fun Compiler.compile(indexed: IndexedAction): Action =
+  when (indexed) {
+    IndexedAction.Empty -> emptyAction
+    is IndexedAction.Capture<*> -> captureAction(state, indexed.variable.typedIndex, indexed.variable.index, evaluator(indexed.value))
+    is IndexedAction.Set<*> -> setAction(state, indexed.variable.typedIndex, indexed.variable.index, evaluator(indexed.value))
+    is IndexedAction.Select -> selectAction(intEvaluator(indexed.index), indexed.actions.map { compile(it) })
+    is IndexedAction.Sequence -> sequenceAction(indexed.actions.map { compile(it) })
+  }
---
+++ src/micapolos/zexy/runtime/Action.kt
@@ -0,0 +1,59 @@
+package micapolos.zexy.runtime
+
+fun interface Action {
+  fun execute()
+}
+
+val emptyAction = Action {}
+
+fun <T> setAction(state: State, typedIndex: Int, index: Int, evaluator: Evaluator<T>): Action =
+  when (evaluator) {
+    is IntEvaluator ->
+      Action {
+        state.evaluatorArray[index] = evaluator
+      }
+
+    is DoubleEvaluator -> {
+      Action {
+        state.evaluatorArray[index] = evaluator
+      }
+    }
+
+    is ObjectEvaluator<T> -> {
+      Action {
+        state.objectArray[typedIndex] = null  // avoids retention
+        state.evaluatorArray[index] = evaluator
+      }
+    }
+  }
+
+fun <T> captureAction(state: State, typedIndex: Int, index: Int, evaluator: Evaluator<T>): Action =
+  when (evaluator) {
+    is IntEvaluator ->
+      Action {
+        state.intArray[typedIndex] = evaluator.eval()
+        state.animatedArray[index] = null
+      }
+
+    is DoubleEvaluator ->
+      Action {
+        state.doubleArray[typedIndex] = evaluator.eval()
+        state.animatedArray[index] = null
+      }
+
+    is ObjectEvaluator<*> ->
+      Action {
+        state.objectArray[typedIndex] = evaluator.eval()
+        state.animatedArray[index] = null
+      }
+  }
+
+fun sequenceAction(actions: List<Action>): Action =
+  Action {
+    actions.forEach(Action::execute)
+  }
+
+fun selectAction(indexEvaluator: IntEvaluator, actions: List<Action>): Action =
+  Action {
+    actions.forEach(Action::execute)
+  }
