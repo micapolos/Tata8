0afd0dec53f04e7bcf1e8a954524ab5ccd46c0da
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-16 00:06:23 +0200

    point / line

--- src/micapolos/zexy/runtime/Animation.kt
+++ src/micapolos/zexy/runtime/Animation.kt
@@ -13,15 +13,6 @@ val instantAnimation: Animation = object : Animation {
   override fun step(seconds: Double): Double = seconds
 }
 
-fun actionAnimation(execute: () -> Unit) =
-  object : Animation {
-    override fun start() {
-      execute()
-    }
-
-    override fun step(seconds: Double): Double = seconds
-  }
-
 fun instantAnimation(action: Action) =
   object : Animation {
     override fun start() {
@@ -54,35 +45,6 @@ fun pauseAnimation(seconds: () -> Double) =
     }
   }
 
-fun Animation.stretch(ratioEvaluator: DoubleEvaluator) =
-  object : Animation {
-    override fun start() {
-      this@stretch.start()
-    }
-
-    override fun step(seconds: Double): Double {
-      return this@stretch.step(seconds * ratioEvaluator.eval())
-    }
-  }
-
-infix fun Animation.then(rhs: Animation) =
-  object : Animation {
-    override fun start() {
-      this@then.start()
-      rhs.start()
-    }
-
-    override fun step(seconds: Double): Double {
-      val lhsSeconds = this@then.step(seconds)
-      val rhsSeconds = rhs.step(seconds)
-      return min(lhsSeconds, rhsSeconds)
-    }
-
-  }
-
-fun parallel(vararg animations: Evaluator<Animation>): Animation =
-  parallel(animations.toList())
-
 fun parallel(animations: List<Evaluator<Animation>>): Animation =
   object : Animation {
     override fun start() {
@@ -98,18 +60,6 @@ fun parallel(animations: List<Evaluator<Animation>>): Animation =
     }
   }
 
-fun everyStepAnimation(animation: Animation): Animation =
-  object : Animation {
-    override fun start() {
-    }
-
-    override fun step(seconds: Double): Double {
-      animation.start()
-      animation.step(seconds)
-      return 0.0
-    }
-  }
-
 fun everyStepAnimation(action: Action): Animation =
   object : Animation {
     override fun start() {
@@ -118,30 +68,6 @@ fun everyStepAnimation(action: Action): Animation =
     override fun step(seconds: Double): Double {
       action.execute()
       return 0.0
-    }
-  }
-
-fun nextFrameAnimation(animation: Animation): Animation =
-  object : Animation {
-    var shouldStart = false
-    var isRunning = false
-
-    override fun start() {
-      shouldStart = false
-      isRunning = false
-    }
-
-    override fun step(seconds: Double): Double {
-      if (!shouldStart) {
-        shouldStart = true
-        return seconds
-      } else {
-        if (!isRunning) {
-          animation.start()
-          isRunning = true
-        }
-        return animation.step(seconds)
-      }
     }
   }
 
@@ -235,68 +161,6 @@ fun Animation.repeatWhile(condition: () -> Boolean): Animation =
     }
   }
 
-fun Animation.runWhileNotZero(condition: () -> Boolean): Animation =
-  object : Animation {
-    override fun start() {
-      this@runWhileNotZero.start()
-    }
-
-    override fun step(seconds: Double): Double =
-      if (condition()) this@runWhileNotZero.step(seconds) else seconds
-  }
-
-fun Animation.startWhenNotZero(intEvaluator: IntEvaluator) =
-  object : Animation {
-    var isRunning = false
-
-    override fun start() {
-      isRunning = false
-    }
-
-    override fun step(seconds: Double): Double {
-      if (intEvaluator.eval() != 0) {
-        this@startWhenNotZero.start()
-        isRunning = true
-      }
-
-      if (isRunning) {
-        this@startWhenNotZero.step(seconds)
-      }
-
-      return 0.0
-    }
-  }
-
-class SelectAnimation(val indexEvaluator: IntEvaluator, val animations: Array<Evaluator<Animation>>) : Animation {
-  override fun start() {
-    animations[indexEvaluator.eval()].evalObject().start()
-  }
-
-  override fun step(seconds: Double): Double {
-    return animations[indexEvaluator.eval()].evalObject().step(seconds)
-  }
-}
-
-class PulseAnimation(val highAnimation: Animation, val lowAnimation: Animation) : Animation {
-  var isLow: Boolean = false
-  private var didStart = false
-
-  override fun start() {
-    didStart = true
-  }
-
-  override fun step(seconds: Double): Double {
-    if (didStart) {
-      isLow = false
-      didStart = false
-      return highAnimation.step(seconds)
-    } else {
-      isLow = true
-      return lowAnimation.step(seconds)
-    }
-  }
-}
-
 fun selectStartAnimation(indexEvaluator: IntEvaluator, animations: List<Evaluator<Animation>>) =
   object : Animation {
     var currentIndex = -1
@@ -331,44 +195,5 @@ fun selectStepAnimation(indexEvaluator: IntEvaluator, animations: List<Evaluator
 
     override fun step(seconds: Double): Double {
       return animations[indexEvaluator.eval()].evalObject().step(seconds)
-    }
-  }
-
-fun <T> captureAnimation(state: State, typedIndex: Int, index: Int,  evaluator: Evaluator<T>) =
-  when (evaluator) {
-    is IntEvaluator ->
-      actionAnimation {
-        state.intArray[typedIndex] = evaluator.eval()
-      }
-
-    is DoubleEvaluator ->
-      actionAnimation {
-        state.doubleArray[typedIndex] = evaluator.eval()
-      }
-
-    is ObjectEvaluator<*> ->
-      actionAnimation {
-        state.objectArray[typedIndex] = evaluator.eval()
-      }
-  }
-
-fun <T> setAnimation(state: State, typedIndex: Int, index: Int,  evaluator: Evaluator<T>): Animation =
-  when (evaluator) {
-    is IntEvaluator ->
-      actionAnimation {
-        state.evaluatorArray[index] = evaluator
-      }
-
-    is DoubleEvaluator -> {
-      actionAnimation {
-        state.evaluatorArray[index] = evaluator
-      }
-    }
-
-    is ObjectEvaluator<T> -> {
-      actionAnimation {
-        state.objectArray[typedIndex] = null  // avoids retention
-        state.evaluatorArray[index] = evaluator
-      }
     }
   }
