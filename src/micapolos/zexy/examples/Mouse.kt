276d4dd5da6b3ecd8b6c0cd4a1b87b53fadd739c
Michał Pociecha-Łoś <micapolos@gmail.com>
2026-09-15 02:29:00 +0200

    point / line

--- src/micapolos/zexy/examples/Mouse.kt
+++ src/micapolos/zexy/examples/Mouse.kt
@@ -3,12 +3,12 @@ package micapolos.zexy.examples
 import micapolos.zexy.*
 
 fun main() {
-//  parallel(
-//    mouse.position.x.loggedAs("x"),
-//    mouse.position.y.loggedAs("y"),
-//    mouse.isPressed.loggedAs("is pressed"),
-//    mouse.press.loggedAs("press"),
-//    mouse.release.loggedAs("release"),
-//  ).show()
+  animation {
+    everyFrame {
+      mouse.position.x logAs "x"
+      mouse.position.y logAs "y"
+      mouse.isPressed logAs "is pressed"
+    }
+  }.show()
 }
 
